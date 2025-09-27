1b2049b977b27fbdfb12f32f39261df16486f5bd
lucas <lucasaita4000@gmail.com>
2025-09-27 05:03:58 -0300

    feat: criando nossa configuracao inicial da entidade/document Email.

---
+++ email-service/src/main/java/dev/lucas/email/entity/Email.java
@@ -0,0 +1,29 @@
+package dev.lucas.email.entity;
+
+import dev.lucas.email.enums.EmailStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Email {
+
+    @Id
+    private UUID id;
+    private String userId;
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+    private LocalDateTime sentAt;
+    private EmailStatus status;
+
+}
---
+++ email-service/src/main/java/dev/lucas/email/enums/EmailStatus.java
@@ -0,0 +1,8 @@
+package dev.lucas.email.enums;
+
+public enum EmailStatus {
+    SENT,
+    ERROR,
+    PENDING,
+    OPENED;
+}
