c243998535d38736d78df1de2ba55fb364f19015
lucas <lucasaita4000@gmail.com>
2025-09-28 04:19:03 -0300

    feat: traz para dentro do Application o dotenv para carregar as variáveis de ambiente

--- email-service/src/main/java/dev/lucas/email/EmailApplication.java
+++ email-service/src/main/java/dev/lucas/email/EmailApplication.java
@@ -1,12 +1,23 @@
 package dev.lucas.email;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmailApplication {
 
+
+
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        // Injeta as variáveis .env do no ambiente do sistema
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
 		SpringApplication.run(EmailApplication.class, args);
 	}
 
