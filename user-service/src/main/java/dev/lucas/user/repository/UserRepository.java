5ca998a52cc5b00e8ceaa089fb49b8747d7facdb
lucas <lucasaita4000@gmail.com>
2025-09-29 14:09:33 -0300

    feat: configura nosso UserRepository para trabalharmos com o JPA

---
+++ user-service/src/main/java/dev/lucas/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.lucas.user.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
