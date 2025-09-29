c0afb352c30969dd9c1302389bbe131fee71b290
lucas <lucasaita4000@gmail.com>
2025-09-29 14:07:43 -0300

    style: adiciona anotacao de @Repository do Spring para definir a interface como repositorio

--- email-service/src/main/java/dev/lucas/email/repository/EmailRepository.java
+++ email-service/src/main/java/dev/lucas/email/repository/EmailRepository.java
@@ -2,8 +2,10 @@ package dev.lucas.email.repository;
 
 import dev.lucas.email.entity.Email;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EmailRepository extends MongoRepository<Email, UUID> {
 }
