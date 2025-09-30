05da5369d07736f04b8c0096e0527aa6e03a5015
lucas <lucasaita4000@gmail.com>
2025-09-30 03:52:55 -0300

    feat: adiciona o dotenv dentro do application para carregar as credencias automaticamente

--- user-service/src/main/java/dev/lucas/user/UserApplication.java
+++ user-service/src/main/java/dev/lucas/user/UserApplication.java
@@ -1,13 +1,23 @@
 package dev.lucas.user;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-	}
+    static {
+        // Carrega o arquivo .env
+        Dotenv dotenv = Dotenv.load();
 
+        // Injeta as variáveis do .env no ambiente do sistema
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 }
