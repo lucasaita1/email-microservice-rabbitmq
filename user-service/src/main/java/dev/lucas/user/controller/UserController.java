c3238b589ef0f4f48b72f8304480ed5714d50e83
lucas <lucasaita4000@gmail.com>
2025-09-30 04:17:55 -0300

    feat: cria o controller para o servico de Users e junto cria o endpoint de criar usuarios totalmente funcional

---
+++ user-service/src/main/java/dev/lucas/user/controller/UserController.java
@@ -0,0 +1,35 @@
+package dev.lucas.user.controller;
+
+import dev.lucas.user.dto.UserRequest;
+import dev.lucas.user.dto.UserResponse;
+import dev.lucas.user.entity.UserEntity;
+import dev.lucas.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/create")
+    ResponseEntity<UserResponse> saveUser(@RequestBody UserRequest user) {
+        var userEntity = new  UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+        userService.saveUser(userEntity);
+        var userResponse = new UserResponse(
+                userEntity.getUserId(),
+                userEntity.getUsername(),
+                userEntity.getEmail());
+        BeanUtils.copyProperties(userEntity, userResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+
+    }
+}
