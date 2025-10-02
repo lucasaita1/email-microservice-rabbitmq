d6f9d0c16b460f1f5a4e9eeafdb430a5de59865f
lucas <lucasaita4000@gmail.com>
2025-10-02 02:57:59 -0300

    feat: adiciona a anotacao @Transactional no método saveUser para servir de rollback em caso de excessoes

--- user-service/src/main/java/dev/lucas/user/service/UserService.java
+++ user-service/src/main/java/dev/lucas/user/service/UserService.java
@@ -2,6 +2,7 @@ package dev.lucas.user.service;
 
 import dev.lucas.user.entity.UserEntity;
 import dev.lucas.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public UserEntity saveUser(UserEntity user) {
         return userRepository.save(user);
     }
