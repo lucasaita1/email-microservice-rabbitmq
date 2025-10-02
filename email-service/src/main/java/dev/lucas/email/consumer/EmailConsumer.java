bd895d2e22fb804779b4b33acf9463752fedaa50
lucas <lucasaita4000@gmail.com>
2025-10-02 04:21:55 -0300

    fix: altera o corpo de String para EmailDto e comecarmos a configurar o ambiente real

--- email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
+++ email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
@@ -1,13 +1,15 @@
 package dev.lucas.email.consumer;
 
+import dev.lucas.email.dto.EmailDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
     @RabbitListener(queues = "email-queue")
-    public void listenEmailQueue(String message){
+    public void listenEmailQueue(@Payload EmailDto message){
         System.out.println(message);
     }
 }
