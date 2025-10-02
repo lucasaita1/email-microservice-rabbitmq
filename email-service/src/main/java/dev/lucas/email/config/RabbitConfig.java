f7f4fda857ff0c4ecae1887aa633f4f215eee0c9
lucas <lucasaita4000@gmail.com>
2025-10-02 04:20:37 -0300

    feat: adiciona o JacksonJsonMessageConverter acompanhando do @Bean para realizar as conversoes de mensagens JSON em Dtos para nosso consumer. Este é o passo inicial para sairmos dos testes e entrar para configuracao de fato do RabbitMQ

--- email-service/src/main/java/dev/lucas/email/config/RabbitConfig.java
+++ email-service/src/main/java/dev/lucas/email/config/RabbitConfig.java
@@ -1,7 +1,9 @@
 package dev.lucas.email.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,15 @@ public class RabbitConfig {
 
     private final String queueName = "email-queue";
 
-    @Bean
+
     public Queue queue() {
         return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
 }
