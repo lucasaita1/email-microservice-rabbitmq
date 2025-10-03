2297c40e07e62b28c1c3279498202dc9e92dedff
lucas <lucasaita4000@gmail.com>
2025-10-03 03:59:42 -0300

    feat: cria um EmailDto para transferirmos os dados do usuario na mensagem em formato de email para que o servico de email possa consumir

---
+++ user-service/src/main/java/dev/lucas/user/dto/EmailDto.java
@@ -0,0 +1,24 @@
+package dev.lucas.user.dto;
+
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+
+@Service
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDto {
+
+    private UUID UserId;
+    private String to;
+    private String subject;
+    private String body;
+}
