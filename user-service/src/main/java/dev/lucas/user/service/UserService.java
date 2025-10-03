2b34521965ff8e847eaf7e15b8a0d38c961d765c
lucas <lucasaita4000@gmail.com>
2025-10-03 04:08:10 -0300

    feat: injeta no método de criar a funcao de criar as mensagens com base no usurio que esta sendo criado. Usa-se a anotation @Transactional para separar as acoes em blocos e garantimos o rollback em caso de erros.

--- user-service/src/main/java/dev/lucas/user/service/UserService.java
+++ user-service/src/main/java/dev/lucas/user/service/UserService.java
@@ -1,6 +1,7 @@
 package dev.lucas.user.service;
 
 import dev.lucas.user.entity.UserEntity;
+import dev.lucas.user.producer.UserProducer;
 import dev.lucas.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
     @Transactional
-    public UserEntity saveUser(UserEntity user) {
-        return userRepository.save(user);
+    public UserEntity saveAndPublish(UserEntity user) {
+        userRepository.save(user);
+        userProducer.sendMensagem(user);
+        return user;
     }
 }
