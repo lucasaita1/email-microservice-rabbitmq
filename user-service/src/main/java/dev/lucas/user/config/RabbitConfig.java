7258c97357c779d1dfa9d81865b7fa59ea4a8237
lucas <lucasaita4000@gmail.com>
2025-10-03 04:06:09 -0300

    fix: corrige o método, simplificando ele e deixando o papel de conexao exclusivo com o application.yml e tornando responsável somente pelo papel de converter as mensagens

--- user-service/src/main/java/dev/lucas/user/config/RabbitConfig.java
+++ user-service/src/main/java/dev/lucas/user/config/RabbitConfig.java
@@ -1,8 +1,10 @@
 package dev.lucas.user.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,25 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     @Bean
-    public CachingConnectionFactory connectionFactory() throws Exception {
-        CachingConnectionFactory factory = new CachingConnectionFactory();
-        factory.setHost(System.getProperty("RABBITMQ_HOST2"));
-        factory.setPort(Integer.parseInt(System.getProperty("RABBITMQ_PORT2")));
-        factory.setUsername(System.getProperty("RABBITMQ_USERNAME2"));
-        factory.setPassword(System.getProperty("RABBITMQ_PASSWORD2"));
-        factory.setVirtualHost(System.getProperty("RABBITMQ_VHOST2"));
-
-        factory.getRabbitConnectionFactory().useSslProtocol();
-        return factory;
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(CachingConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
-    }
-
-    @Bean
-    public Queue testeQueue() {
-        return new Queue("teste", true);
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
