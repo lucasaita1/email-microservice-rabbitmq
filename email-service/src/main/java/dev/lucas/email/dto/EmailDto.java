922563f7b1317f51b205ef2fffed7246a31e6e29
lucas <lucasaita4000@gmail.com>
2025-10-04 03:18:33 -0300

    fix: arruma erros na escrita e compilacao dos dtos

--- email-service/src/main/java/dev/lucas/email/dto/EmailDto.java
+++ email-service/src/main/java/dev/lucas/email/dto/EmailDto.java
@@ -1,8 +1,11 @@
 package dev.lucas.email.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.UUID;
 
-public record EmailDto(UUID UserId,
+
+public record EmailDto(UUID userId,
                        String to,
                        String subject,
                        String body){
--- user-service/src/main/java/dev/lucas/user/dto/UserResponse.java
+++ user-service/src/main/java/dev/lucas/user/dto/UserResponse.java
@@ -1,7 +1,10 @@
 package dev.lucas.user.dto;
 
+import lombok.Builder;
+
 import java.util.UUID;
 
+@Builder
 public record UserResponse(UUID userId,
                            String username,
                            String email) {
