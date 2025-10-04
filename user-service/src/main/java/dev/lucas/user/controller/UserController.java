a9b6b3c73d3b7013b955734f559bcbd4f1813ae0
lucas <lucasaita4000@gmail.com>
2025-10-04 03:16:18 -0300

    feat: cria todos os endpoints no controller de uma api REST

--- user-service/src/main/java/dev/lucas/user/controller/UserController.java
+++ user-service/src/main/java/dev/lucas/user/controller/UserController.java
@@ -3,6 +3,7 @@ package dev.lucas.user.controller;
 import dev.lucas.user.dto.UserRequest;
 import dev.lucas.user.dto.UserResponse;
 import dev.lucas.user.entity.UserEntity;
+import dev.lucas.user.mapper.UserMapper;
 import dev.lucas.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -10,6 +11,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
@@ -32,4 +36,33 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
 
     }
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserEntity> entity = userService.findAll();
+        List<UserResponse> response = entity.stream()
+                .map(userEntity -> UserMapper.toResponse(userEntity))
+                .toList();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        return userService.findById(id)
+                .map(user -> ResponseEntity.status(HttpStatus.OK).body(UserMapper.toResponse(user)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<UserResponse> updateUserById(@PathVariable UUID id, @RequestBody UserRequest user) {
+        return userService.updateById(id, UserMapper.toEntity(user))
+                .map(updatedUser -> ResponseEntity.status(HttpStatus.OK).body(UserMapper.toResponse(updatedUser)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteUserById(@PathVariable UUID id) {
+        userService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
