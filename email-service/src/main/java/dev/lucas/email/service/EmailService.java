b11bef9560021197515fe6425e81897ffa9abf9c
lucas <lucasaita4000@gmail.com>
2025-10-05 17:12:09 -0300

    feat: cria a conexao e o método SMTP para o nosso servico de email consumir a queue de dados gerados pelo users e com isso manda um email automatico usando os dados fornecidos via mensageria

--- email-service/src/main/java/dev/lucas/email/service/EmailService.java
+++ email-service/src/main/java/dev/lucas/email/service/EmailService.java
@@ -1,14 +1,51 @@
 package dev.lucas.email.service;
 
 
+import dev.lucas.email.entity.Email;
+import dev.lucas.email.enums.EmailStatus;
 import dev.lucas.email.repository.EmailRepository;
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final EmailRepository emailRepository;
+    private final JavaMailSender mailSender;
 
+
+    Dotenv dotenv = Dotenv.load();
+
+    private String emailFrom;
+
+    {
+        emailFrom = dotenv.get("EMAIL_USERNAME");
+    }
+
+
+    @Transactional
+    public void sendEmail(Email emailModel) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailFrom);
+            message.setTo(emailModel.getTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getBody());
+
+            mailSender.send(message);
+
+            emailModel.setStatus(EmailStatus.SENT);
+        } catch (Exception e) {
+            emailModel.setStatus(EmailStatus.ERROR);
+            System.out.println("Erro ao enviar email: " + e.getMessage());
+        } finally {
+            emailRepository.save(emailModel);
+        }
+    }
 }
