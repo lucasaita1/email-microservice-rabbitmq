4097af4c4ab5a4b1af65a6e90d7429eadc21684a
lucas <lucasaita4000@gmail.com>
2025-10-05 17:00:41 -0300

    feat: EmailConsumer recebe do Service a funcao de enviar emails usando o consumo da mensageria

--- email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
+++ email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
@@ -1,15 +1,26 @@
 package dev.lucas.email.consumer;
 
 import dev.lucas.email.dto.EmailDto;
+import dev.lucas.email.entity.Email;
+import dev.lucas.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+
+    private final EmailService emailService;
 
     @RabbitListener(queues = "email-queue")
     public void listenEmailQueue(@Payload EmailDto message){
-        System.out.println(message);
+       var emailModel = new Email();
+        BeanUtils.copyProperties(message, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
