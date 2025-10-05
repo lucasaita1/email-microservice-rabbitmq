8c70a8bc0f8b04ac6fc285b6c48238e65c31fc5f
lucas <lucasaita4000@gmail.com>
2025-10-05 16:51:13 -0300

    fix: corrige erro na persistencia dos dados no mongo db, onde o mesmo nao estava conseguindo gerar um UUID AUTO

--- email-service/src/main/java/dev/lucas/email/dto/EmailDto.java
+++ email-service/src/main/java/dev/lucas/email/dto/EmailDto.java
@@ -1,11 +1,7 @@
 package dev.lucas.email.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.UUID;
-
-
-public record EmailDto(UUID userId,
+public record EmailDto(String userId,
                        String to,
                        String subject,
                        String body){
