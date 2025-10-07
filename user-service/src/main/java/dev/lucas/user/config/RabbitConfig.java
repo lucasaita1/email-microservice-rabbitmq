389a9a5c33ccfda6c2388807d056340b93e8c95a
lucas <lucasaita4000@gmail.com>
2025-10-07 03:34:59 -0300

    remove: remove imports desnecessários deixando o código limpo

--- user-service/src/main/java/dev/lucas/user/config/RabbitConfig.java
+++ user-service/src/main/java/dev/lucas/user/config/RabbitConfig.java
@@ -1,9 +1,6 @@
 package dev.lucas.user.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
