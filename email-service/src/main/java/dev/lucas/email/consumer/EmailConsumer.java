d4876aff197981717edc8578f155498d81e1c176
lucas <lucasaita4000@gmail.com>
2025-10-07 03:34:03 -0300

    remove: remove imports desnecessários deixando o código limpo

--- email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
+++ email-service/src/main/java/dev/lucas/email/consumer/EmailConsumer.java
@@ -6,7 +6,6 @@ import dev.lucas.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
